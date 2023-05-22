ab5c75e922bae1f304a2e046453cafdd1ec3e16a
nagadit <a.o.nagaev@yandex.ru>
2023-05-22 16:32:15 +0300

    added onnx demo for SLOVO

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Slovo - Russian Sign Language Dataset
 
-We introduce a large-scale video dataset **Slovo** for Russian Sign Language task. Slovo dataset size is about **14.77 GB**, and it contains **20400** RGB videos for **1000** sign language gestures. Each class has 20 samples. The dataset is divided into training set and test set by subject `user_id`. The training set includes 15300 videos, and the test set includes 5100 videos. The total video recording time is 9.51 hours, 35% of the videos are recorded in HD format, and 65% of the videos are in FullHD resolution. The average video length with gesture is 50 frames.
+We introduce a large-scale video dataset **Slovo** for Russian Sign Language task. Slovo dataset size is about **16 GB**, and it contains **20400** RGB videos for **1000** sign language gestures from 194 singers. Each class has 20 samples. The dataset is divided into training set and test set by subject `user_id`. The training set includes 15300 videos, and the test set includes 5100 videos. The total video recording time is ~9.2 hours. About 35% of the videos are recorded in HD format, and 65% of the videos are in FullHD resolution. The average video length with gesture is 50 frames.
 
 For more information see our paper - [arXiv]().
 
 ## Downloads
 ### [Download Link](https://n-ws-620xz-pd11.s3pd11.sbercloud.ru/b-ws-620xz-pd11-jux/slovo/slovo.zip)
 
-Also, you can download it from [Kaggle](https://www.kaggle.com/datasets/kapitanov/slovo).  
+Also, you can download it from [Kaggle](https://www.kaggle.com/datasets/kapitanov/slovo).
 
 Annotation file is easy to use and contains some useful columns, see `annotations.csv` file:
 
-|    | attachment_id | user_id | width | height | length |  text  | train   |
-|---:|:--------------|:--------|------:|-------:|-------:|-------:|:--------|
-|  0 | de81cc1c-...  | 1b...   |  1440 |   1920 |     14 | привет | True    |
-|  1 | 3c0cec5a-...  | 64...   |  1440 |   1920 |     32 |   утро | False   |
-|  2 | d17ca986-...  | cf...   |  1920 |   1080 |     44 |  улица | False   |
+|    | attachment_id | user_id | width | height | length |  text  | train   | begin | end |
+|---:|:--------------|:--------|------:|-------:|-------:|-------:|:--------|:------|:----|
+|  0 | de81cc1c-...  | 1b...   |  1440 |   1920 |     14 | привет | True    | 30    | 45  |
+|  1 | 3c0cec5a-...  | 64...   |  1440 |   1920 |     32 |   утро | False   | 43    | 66  |
+|  2 | d17ca986-...  | cf...   |  1920 |   1080 |     44 |  улица | False   | 12    | 31  |
 
 where:
 - `attachment_id` - video file name
@@ -25,6 +25,10 @@ where:
 - `length` - video length
 - `text` - gesture class in Russian Langauge
 - `train` - train or test boolean flag
+- `begin` - start of the gesture (for original dataset)
+- `end` - end of the gesture (for original dataset)
+
+For convenience, we have also prepared a compressed version of the dataset, in which all videos are processed by the minimum side `min_side = 360`. Download link - **[slovo360p](https://n-ws-620xz-pd11.s3pd11.sbercloud.ru/b-ws-620xz-pd11-jux/slovo/slovo360.zip)**.
 
 ## Models
 We provide some pre-trained models as the baseline for russian sign language recognition.
@@ -35,7 +39,7 @@ We provide some pre-trained models as the baseline for russian sign language rec
 
 ## Demo
 ```console
-python slovo.py -p <PATH_TO_CHECKPOINT>
+python demo.py -p <PATH_TO_CONFIG>
 ```
 
 ## Authors and Credits
@@ -53,6 +57,14 @@ You can cite the paper using the following BibTeX entry:
         journal={arXiv preprint <link>},
         year={2023}
     }
+
+## Links
+- [arXiv]()
+- [Kaggle](https://www.kaggle.com/datasets/kapitanov/slovo)
+- [Habr]()
+- [Medium]()
+- [Gitlab](https://gitlab.aicloud.sbercloud.ru/rndcv/slovo)
+- [Paperswithcode]()
 
 ## License
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a variant of <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
---
+++ demo.py
@@ -0,0 +1,196 @@
+import argparse
+import logging
+import time
+from collections import deque
+from multiprocessing import Manager, Process, Value
+from typing import Optional, Tuple
+
+import onnxruntime as ort
+
+ort.set_default_logger_severity(4)
+import cv2
+import numpy as np
+from omegaconf import OmegaConf
+
+from constants import classes
+
+
+class Recognition(Process):
+    def __init__(self, model_path: str, tensors_list, prediction_list):
+        """
+        Initialize recognition model.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the model.
+        tensors_list : Manager.list
+            List of tensors to be used for prediction.
+        prediction_list : Manager.list
+            List of predictions.
+
+        Notes
+        -----
+        The recognition model is run in a separate process.
+        """
+        super().__init__()
+        self.started = Value("i", False)
+        self.model_path = model_path
+        self.window_size = None
+        self.tensors_list = tensors_list
+        self.prediction_list = prediction_list
+
+    def clear(self):
+        """
+        Clear the list of tensors.
+        """
+        for _ in range(self.window_size):
+            self.tensors_list.pop(0)
+
+    def run(self):
+        """
+        Run the recognition model.
+        """
+        self.session = ort.InferenceSession(self.model_path)
+        self.input_name = self.session.get_inputs()[0].name
+        self.input_shape = self.session.get_inputs()[0].shape
+        self.window_size = self.input_shape[3]
+        self.output_names = [output.name for output in self.session.get_outputs()]
+        while True:
+            self.started = True
+            if len(self.tensors_list) >= self.input_shape[3]:
+                input = np.stack(self.tensors_list[: self.input_shape[3]], axis=1)[None][None]
+                st = time.time()
+                outputs = self.session.run(self.output_names, {self.input_name: input.astype(np.float32)})
+                logging.info(f" --- prediction time {time.time() - st}")
+                clas = str(classes[outputs[0].argmax()])
+                if clas not in self.prediction_list:
+                    self.prediction_list.append(clas)
+                self.clear()
+                logging.info(f" --- {len(self.tensors_list)} frames in queue")
+
+
+class Runner:
+    def __init__(self, model_path: str, config) -> None:
+        """
+        Initialize runner.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the model.
+        config : OmegaConf
+            Configuration file.
+
+        Notes
+        -----
+        The runner uses multiprocessing to run the recognition model in a separate process.
+
+        """
+        self.cap = cv2.VideoCapture(0)
+        self.manager = Manager()
+        self.tensors_list = self.manager.list()
+        self.prediction_list = self.manager.list()
+        self.frame_counter = 0
+        self.frame_interval = config.frame_interval
+        self.prediction_classes = deque(maxlen=4)
+        self.mean = config.mean
+        self.std = config.std
+        self.recognizer = Recognition(model_path, self.tensors_list, self.prediction_list)
+
+    def add_frame(self, image):
+        """
+        Add frame to queue.
+
+        Parameters
+        ----------
+        image : np.ndarray
+            Frame to be added.
+        """
+        self.frame_counter += 1
+        if self.frame_counter == self.frame_interval:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = self.resize(image, (224, 224))
+            image = (image - self.mean) / self.std
+            image = np.transpose(image, [2, 0, 1])
+            self.tensors_list.append(image)
+            self.frame_counter = 0
+
+    @staticmethod
+    def resize(im, new_shape=(224, 224)):
+        """
+        Resize and pad image while preserving aspect ratio.
+
+        Parameters
+        ----------
+        im : np.ndarray
+            Image to be resized.
+        new_shape : Tuple[int]
+            Size of the new image.
+
+        Returns
+        -------
+        np.ndarray
+            Resized image.
+        """
+        shape = im.shape[:2]  # current shape [height, width]
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio (new / old)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+
+        # Compute padding
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+        dw /= 2
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(114, 114, 114))  # add border
+        return im
+
+    def run(self):
+        """
+        Run the runner.
+
+        Notes
+        -----
+        The runner will run until the user presses 'q'.
+        """
+        self.recognizer.start()
+        while self.cap.isOpened():
+            if self.recognizer.started:
+                _, frame = self.cap.read()
+                text_div = np.zeros((50, frame.shape[1], 3), dtype=np.uint8)
+                self.add_frame(frame)
+                if len(self.prediction_list):
+                    self.prediction_classes.extend(self.prediction_list)
+                    text = " ".join(self.prediction_classes)
+                    cv2.putText(text_div, text, (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 1)
+                frame = np.concatenate((frame, text_div), axis=0)
+                cv2.imshow("frame", frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    self.recognizer.kill()
+                    break
+
+
+def parse_arguments(params: Optional[Tuple] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Demo full frame classification...")
+
+    parser.add_argument("-p", "--config", required=True, type=str, help="Path to config")
+
+    known_args, _ = parser.parse_known_args(params)
+    return known_args
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    args = parse_arguments()
+    conf = OmegaConf.load(args.config)
+    runner = Runner(conf.model_path, conf)
+    runner.run()
